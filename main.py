93e8ef321bf37b85d12fc211eebe49a58c5b506c
somecallmetim <timbauer@ymail.com>
2015-06-22 15:07:35 -0700

    first comit

---
+++ main.py
@@ -0,0 +1,19 @@
+__author__ = 'timbauer'
+
+import csv
+
+
+
+teams = []
+
+# teamName = (raw_input("Select Team initials: "))
+with open('LoLData.csv', 'rU') as csvfile:
+    reader = csv.DictReader(csvfile)
+#    for row in reader:
+#        if teamName == row['Team']:
+#            print(row['Name'], row['Team'])
+    for row in reader:
+        if not (row['Team'] in teams):
+            teams.append(row['Team'])
+
+    print teams
